77d1032a23d4c7abf2a8c97f8fb32ddc3863bd4c
Rodolfo Delduca <usodelduca@gmail.com>
2023-10-26 16:33:09 +1300

    add POST to form

--- index.php
+++ index.php
@@ -1,8 +1,45 @@
 <?php include 'inc/header.php'; ?>
+
+
+<?php 
+  $name = $email = $body = '';
+  $nameErr = $emailErr = $bodyErr = '';
+
+      // FORM submit //
+
+      if(isset($_POST['submit'])){
+        // validate name
+
+        if(empty($_POST['name'])){
+          $nameErr = 'Name is required';
+        } else {
+          $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        }
+
+        // validate email
+
+        if(empty($_POST['email'])){
+          $emailErr = 'Email is required';
+        } else {
+          $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+        }
+
+        // validate body
+
+        if(empty($_POST['body'])){
+          $bodyErr = 'Feedback is required';
+        } else {
+          $body = filter_input(INPUT_POST, 'body', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        }
+      }
+
+      
+?>
+
         <img src="/php-crash/feedback-app-php/img/Koko.jpg" class="w-25 mb-3" alt="" />
         <h2>Feedback</h2>
         <p class="lead text-center">Leave feedback for Traversy Media</p>
-        <form action="" class="mt-4 w-75">
+        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method='POST'  class="mt-4 w-75">
           <div class="mb-3">
             <label for="name" class="form-label">Name</label>
             <input
